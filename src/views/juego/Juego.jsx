dc0432baf1f4271bdfcb84cf70e740c7c911f7f0
franciscoG98 <franciscomanuel.gonzalez3@gmail.com>
2023-06-19 18:07:15 -0300

    fetch compradores and get juego by id from backend to show compradores and juego in Juego view

--- src/views/juego/Juego.jsx
+++ src/views/juego/Juego.jsx
@@ -1,53 +1,160 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import mockJuegos from "../juegos/mockJuegos.js";
-import mockCompradores from "./mockCompradores.js";
 import "./juegoStyles.scss";
 
 const Juego = () => {
-    const [comprador, setComprador] = useState();
+    const [comprador, setComprador] = useState()
+    const [compradores, setCompradores] = useState([
+        {
+        "idComprador": 1,
+        "nombreComprador": "Juan",
+        "apellidoComprador": "Pérez",
+        "dniComprador": "12345678",
+        "correoElectronicoComprador": "juan.perez@example.com",
+        "paseDeOro": true
+        },
+        {
+        "idComprador": 2,
+        "nombreComprador": "María",
+        "apellidoComprador": "González",
+        "dniComprador": "87654321",
+        "correoElectronicoComprador": "maria.gonzalez@example.com",
+        "paseDeOro": false
+        },
+        {
+        "idComprador": 3,
+        "nombreComprador": "Pedro",
+        "apellidoComprador": "Rodríguez",
+        "dniComprador": "98765432",
+        "correoElectronicoComprador": "pedro.rodriguez@example.com",
+        "paseDeOro": true
+        },
+    ])
 
-    const { id } = useParams();
+    const [juego, setJuego] = useState({
+        "idJuego": 1,
+        "nombreJuego": "Montaña Rusa",
+        "precioJuego": 10,
+        "cobroPaseOro": true,
+        "horarios": [
+            {
+                "idHorarioJuego": 1,
+                "horaInicio": "2023-06-19T16:00:00",
+                "horaFin": "2023-06-19T20:00:00"
+            },
+            {
+                "idHorarioJuego": 2,
+                "horaInicio": "2023-06-20T16:00:00",
+                "horaFin": "2023-06-20T20:00:00"
+            },
+            {
+                "idHorarioJuego": 3,
+                "horaInicio": "2023-06-21T16:00:00",
+                "horaFin": "2023-06-21T20:00:00"
+            }
+        ]
+    })
 
-    const juegoSeleccionado = mockJuegos.filter((juego) => juego.id == id);
-
-    const juego = juegoSeleccionado[0];
+    useEffect(() => {
+        handleGetCompradores()
+        handleGetJuego()
+    }, [])
 
     const handleSell = () => {
         alert(`Entrada vendida a ${comprador}`);
     };
 
+    const getCompradoresURL = 'http://localhost:8080/compradores'
+
+    const handleGetCompradores = async() => {
+        try {
+            fetch(getCompradoresURL, {
+                method: "GET",
+                headers: { "Content-Type": "application/json" }
+              })
+              .then( async (response) => {
+                  if(response.ok) {
+                    const res = await response.json()
+                    // console.log('compradores ', res);
+                    setCompradores(res)
+                  } else {
+                    console.log('Hay un error no se donde')
+                  }
+                })
+              .catch((err) => {
+                console.log('Error: ', err)
+              })
+        } catch (error) {
+            console.log('Hubo un error: ', error)
+        }
+    }
+    
+    const { id } = useParams();
+    const getJuegosURL = `http://localhost:8080/juegos/${id}`
+
+    const handleGetJuego = async() => {
+        try {
+            fetch(getJuegosURL, {
+                method: "GET",
+                headers: { "Content-Type": "application/json" }
+              })
+              .then( async (response) => {
+                  if(response.ok) {
+                    const res = await response.json()
+                    // console.log('juego ', res);
+                    setJuego(res)
+                  } else {
+                    console.log('Hay un error no se donde')
+                  }
+                })
+              .catch((err) => {
+                console.log('Error: ', err)
+              })
+        } catch (error) {
+            console.log('error: ', error)
+        }
+    }
+
     return (
         <main className="juego_container">
             <section className="juego">
-                <h1 className="juego_title">{juego.nombre}</h1>
-                <img
+                <h1 className="juego_title">{juego.nombreJuego}</h1>
+                {/* <img
                     className="juego_img"
                     src={juego.foto}
-                    alt={juego.nombre}
-                />
-                <p className="juego_description">{juego.descripcion}</p>
-                <span>Horario: {juego.horario}</span>
-                <span>Capacidad: {juego.capacidad}</span>
-                <span>precio: $$$</span>
+                    alt={juego.nombreJuego}
+                /> */}
+                {/* <p className="juego_description">{juego.descripcion}</p> */}
+                {/* {console.log('horario', juego.horarios)} */}
+                {/* <span>Horario: {juego.horarios}</span> */}
+                <p>Horarios: <br />
+                    {juego.horarios.map( i => (
+                        <span key={i.idHorarioJuego}>{i.horaInicio + " "} - {i.horaFin + " "} <br /></span>
+                    ))}
+                </p>
+                {/* <span>Capacidad: {juego.capacidad}</span> */}
+                <span>precio: ${juego.precioJuego}</span>
             </section>
             <section className="comprador">
-                <select
-                    className="select_buyer"
-                    id="comprador"
-                    name="comprador"
-                    value={comprador}
-                    onChange={(event) => setComprador(event.target.value)}
-                >
-                    {mockCompradores.map((comprador) => (
-                        <option key={comprador.id} value={comprador.nombre + " " + comprador.apellido}>
-                            {comprador.nombre + " " + comprador.apellido}
-                        </option>
-                    ))}
-                </select>
-                <button className="sell_game_btn" onClick={handleSell}>
-                    Vender
-                </button>
+                <form>
+                    <select
+                        className="select_buyer"
+                        id="comprador"
+                        name="comprador"
+                        value={comprador}
+                        onChange={(event) => setComprador(event.target.value)}
+                    >
+                        {compradores.map((c) => (
+                            <option key={c.idComprador} value={c.nombreComprador + " " + c.apellidoComprador}>
+                                {c.nombreComprador + " " + c.apellidoComprador}
+                            </option>
+                        ))}
+                    </select>
+                    <br />
+                    <button className="sell_game_btn" onClick={handleSell} type="submit">
+                        Vender
+                    </button>
+                </form>
             </section>
         </main>
     );
