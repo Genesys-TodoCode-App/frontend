c221a9145aceb69187e3c7a1fec905b8bde76313
franciscoG98 <franciscomanuel.gonzalez3@gmail.com>
2023-06-23 14:15:07 -0300

    ventas view done

--- src/views/ventas/Ventas.jsx
+++ src/views/ventas/Ventas.jsx
@@ -21,11 +21,9 @@ const Ventas = () => {
                 headers: { "Content-Type": "application/json" }
             })
             .then( async (response) => {
-                console.log("res", response);
                 if(response.ok) {
                     const res = await response.json()
                     setVentas(res)
-                    console.log('ventas', ventas);
                 } else {
                     console.log('Hay un error')
                 }
@@ -46,19 +44,16 @@ const Ventas = () => {
                 <thead>
                     <tr>
                         <th>Fecha de Venta</th>
-                        <th>Vendedor</th>
                         <th>Juego</th>
                         <th>Precio de juego</th>
                     </tr>
                 </thead>
                 <tbody>
                     {ventas.map( i => (
-                        <tr key={i.idVentaEntrada}>
-                            {/* {console.log('item: ', i)} */}
-                            <td>{i.entrada.fechaHoraUtilizacion}</td>
-                            <td>{i.empleado.nombreEmpleado + ' ' + i.empleado.apellidoEmpleado}</td>
-                            <td>{i.entrada.juego.nombreJuego}</td>
-                            <td>{i.entrada.juego.precioJuego}</td>
+                        <tr key={i["Id Venta Entradas"]}>
+                            <td>{i.entrada["Fecha y Hora Utilizacion"]}</td>
+                            <td>{i.entrada.juego["Nombre Juegos"]}</td>
+                            <td>{i.entrada.juego["Precio Juegos"]}</td>
                         </tr>
                     ))}
                 </tbody>
